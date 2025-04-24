7d80c3c593947abf60d39a7423d91ab59cadeef2
Afiq Amjad bin Khairir <afiqk@bu.edu>
2025-04-24 19:26:32 -0400

    Update 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import NavigationBar from "../../components/navigation";
 import Image from "next/image";
-import Link from "next/link";
 import Footer from "../../components/footer";
 export default function Home() {
   return (
