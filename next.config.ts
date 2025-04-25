3350bd1ff1e0ab29fda9a7720466f634a2be8081
Afiq Amjad bin Khairir <afiqk@bu.edu>
2025-04-24 20:56:13 -0400

    Removed unoptimized

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     images: {
-      unoptimized: true,
       domains: ['res.cloudinary.com',
         'img.icons8.com', 
       ],
