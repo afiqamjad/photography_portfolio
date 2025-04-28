77b2d7f214691c1fb58867fe70ce544b9dd706b7
Afiq Amjad bin Khairir <afiqk@bu.edu>
2025-04-27 21:24:55 -0400

    Added x button for easy mobile exit from lightbox

--- components/lightbox.tsx
+++ components/lightbox.tsx
@@ -25,7 +25,8 @@ export default function Lightbox({ src, alt, className, width, height}: { src: s
         className={`cursor-pointer ${className}`}
         onClick={openLightbox}
       />
-      <dialog ref={dialogRef} onClick={closeLightbox} className="m-auto max-w-screen max-h-screen border-0 p-0 bg-transparent w-[90vw] h-[90vh] backdrop:bg-black/80 focus:outline-none outline-none" >
+      <dialog ref={dialogRef} onClick={closeLightbox} className="m-auto max-w-screen max-h-screen border-0 p-0 bg-transparent w-[90vw] h-[99vh] backdrop:bg-black/80 focus:outline-none outline-none" >
+            <button className='sm:hidden text-md text-white h-[1.5rem] w-[1.5rem] border-1 rounded-lg border-white text-center absolute top-5 active:bg-gray-50/60'>X</button>
             <Image onClick={e => e.stopPropagation()} fill objectFit='contain' src={src} alt="Enlarged view" className='max-w-fit max-h-fit translate-x-[-50%] translate-y-[-50%]' style={{left: '50%', top: '50%'}}/>
       </dialog>
     </>
