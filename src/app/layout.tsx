6a1be50402fe7eaba6c522b46719ae7cc7f73a74
Afiq Amjad bin Khairir <afiqk@bu.edu>
2025-04-27 21:41:38 -0400

    Fixed viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poiret_One } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import type { Viewport } from "next";
 
 const poiretOne = Poiret_One({
   weight: "400",
@@ -14,6 +15,13 @@ export const metadata: Metadata = {
   title: "Afiq's Photography Portfolio",
   description: "Remember me by the pictures that I take.",
 };
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
 
 export default function RootLayout({
   children,
