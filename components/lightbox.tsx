b82836701623c2b9f6d971d00d62c89da07bb80d
Afiq Amjad bin Khairir <afiqk@bu.edu>
2025-04-29 11:29:01 -0400

    Changed breakpoint and reverted X button position

--- components/lightbox.tsx
+++ components/lightbox.tsx
@@ -25,7 +25,7 @@ export default function Lightbox({ src, alt, className, width, height}: { src: s
         onClick={openLightbox}
       />
       <dialog ref={dialogRef} onClick={closeLightbox} className="m-auto max-w-screen max-h-screen border-0 p-0 bg-transparent w-[90vw] h-[99vh] backdrop:bg-black/85 focus:outline-none outline-none" >
-            <button className='md:hidden z-50 text-md text-white h-[1.5rem] w-[1.5rem] border-1 rounded-lg border-white text-center absolute top-5 left-[50%] active:bg-gray-50/60 focus:outline-none'>X</button>
+            <button className='lg:hidden z-50 text-md text-white h-[1.5rem] w-[1.5rem] border-1 rounded-lg border-white text-center absolute top-5 active:bg-gray-50/60 focus:outline-none'>X</button>
             <Image onClick={e => e.stopPropagation()} fill objectFit='contain' src={src} alt="Enlarged view" className='max-w-fit max-h-fit translate-x-[-50%] translate-y-[-50%]' style={{left: '50%', top: '50%'}}/>
       </dialog>
     </>
