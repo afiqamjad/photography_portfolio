f805fdec002cd2d8c1cc1e8e1e5c5eb862972d91
Afiq Amjad bin Khairir <afiqk@bu.edu>
2025-04-29 12:01:43 -0400

    Changed to relative positioning (X Button)

--- components/lightbox.tsx
+++ components/lightbox.tsx
@@ -24,8 +24,8 @@ export default function Lightbox({ src, alt, className, width, height}: { src: s
         className={`cursor-pointer ${className}`}
         onClick={openLightbox}
       />
-      <dialog ref={dialogRef} onClick={closeLightbox} className="m-auto max-w-screen max-h-screen border-0 p-0 bg-transparent w-[90vw] h-[99vh] backdrop:bg-black/85 focus:outline-none outline-none" >
-            <button className='lg:hidden z-50 text-md text-white h-[1.5rem] w-[1.5rem] border-1 rounded-lg border-white text-center absolute top-5 active:bg-gray-50/60 focus:outline-none'>X</button>
+      <dialog ref={dialogRef} onClick={closeLightbox} className="m-auto max-w-screen max-h-screen border-0 p-0 bg-transparent w-[90vw] h-[99vh] backdrop:bg-black/85 focus:outline-none outline-none absolute" >
+            <button className='lg:hidden z-50 text-md text-white h-[1.5rem] w-[1.5rem] border-1 rounded-lg border-white text-center relative top-[12%] active:bg-gray-50/60 focus:outline-none'>X</button>
             <Image onClick={e => e.stopPropagation()} fill objectFit='contain' src={src} alt="Enlarged view" className='max-w-fit max-h-fit translate-x-[-50%] translate-y-[-50%]' style={{left: '50%', top: '50%'}}/>
       </dialog>
     </>
